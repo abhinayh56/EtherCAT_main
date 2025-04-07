7b6e9577f87e1f32120b5b38a620466955b9e307
abhinayh56 <kumarabhinay53@gmail.com>
2025-04-07 16:21:47 +0530

    temp commit

--- EtherCAT_layer/Slaves/EtherCAT_slave_1.h
+++ EtherCAT_layer/Slaves/EtherCAT_slave_1.h
@@ -10,49 +10,20 @@ public:
     EtherCAT_slave_1() {};
     
     ~EtherCAT_slave_1() {}
-    
-
-    // void set_slave_name(const std::string &slave_name_) {}
-
-    // std::string &get_slave_name() const {}
-
-    // void set_slave_address(const uint16_t slave_address_) {}
-
-    // uint16_t get_slave_address() const {}
-
-    // void set_slave_info() {}
-
-    // void get_slave_info() {}
-
-    // void config_slave(ec_master_t *master) {}
-
-    // bool is_connected() {}
 
     virtual void register_tx_pdo() {}
 
     virtual void register_rx_pdo() {}
 
-    // void monitor_status() {}
-
-    // void transfer_tx_pdo() {}
-
-    // void transfer_rx_pdo() {}
+    virtual void register_data_transfer() {}
 
     virtual void process_tx_pdo() {}
 
     virtual void process_rx_pdo() {}
 
-    virtual void register_data_transfer() {}
-
     virtual void publish_data() {}
 
     virtual void subscribe_data() {}
-
-protected:
-    // bool connection_status = false;
-    // ec_slave_config_t *sc;
-    // ec_slave_config_state_t sc_state;
-    // uint8_t *domain_1_pd = NULL;
 };
 
 #endif // ETHERCAT_SLAVE_1_H
