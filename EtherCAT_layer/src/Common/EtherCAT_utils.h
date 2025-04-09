f054e81e7a95a71fb86e21352618174445abc0d7
abhinayh56 <kumarabhinay53@gmail.com>
2025-04-09 18:39:07 +0530

    temp commit

--- EtherCAT_layer/src/Common/EtherCAT_utils.h
+++ EtherCAT_layer/src/Common/EtherCAT_utils.h
@@ -15,4 +15,16 @@ struct Pdo_info
     bool is_supported = false;
 };
 
+struct Slave_info
+{
+    uint16_t alias;
+    uint16_t position;
+    uint32_t vendor_id;
+    uint32_t product_code;
+
+    ec_pdo_entry_info_t *slave_pdo_entries;
+    ec_pdo_info_t *slave_pdos;
+    ec_sync_info_t *slave_syncs;
+};
+
 #endif // ETHERCAT_UTILS_H
--- EtherCAT_layer/src/EtherCAT_slave_base/EtherCAT_slave_base.h
+++ EtherCAT_layer/src/EtherCAT_slave_base/EtherCAT_slave_base.h
@@ -9,18 +9,6 @@
 #include "../Common/EtherCAT_utils.h"
 #include "../Logger/Logger_console/Logger_console.h"
 
-struct Slave_info
-{
-    uint16_t alias;
-    uint16_t position;
-    uint32_t vendor_id;
-    uint32_t product_code;
-
-    ec_pdo_entry_info_t *slave_pdo_entries;
-    ec_pdo_info_t *slave_pdos;
-    ec_sync_info_t *slave_syncs;
-};
-
 class EtherCAT_slave_base : public Data_transfer
 {
 public:
